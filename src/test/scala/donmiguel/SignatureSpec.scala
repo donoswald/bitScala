19de7b6eaa2f08ba84485c29e53bfa521171554a
Michael <don.miguel@gmx.ch>
2019-04-16 12:52:29 +0200

    serialize/deserialize signature

---
+++ src/test/scala/donmiguel/SignatureSpec.scala
@@ -0,0 +1,22 @@
+package donmiguel
+
+import scala.util.Random
+
+class SignatureSpec extends UnitSpec {
+
+  it should "serialize and deserializt" in {
+
+    val r = Random.nextInt(Math.pow(2, 256).toInt)
+    val s = Random.nextInt(Math.pow(2, 256).toInt)
+
+    val original = new Signature(r, s)
+
+    var der = original.der()
+
+    var reconstructed = Signature.parse(original.der)
+
+    assert(original==reconstructed)
+
+  }
+
+}
