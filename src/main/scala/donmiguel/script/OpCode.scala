c0f6cf6e648f1426fcfd5001491a1ccc82e606ac
Michael Oswald <michael@oswaldzh.ch>
2019-04-29 13:06:30 +0200

    opcodes encode/decode + tests

--- src/main/scala/donmiguel/script/OpCode.scala
+++ src/main/scala/donmiguel/script/OpCode.scala
@@ -1,8 +1,76 @@
 package donmiguel.script
 
-object OpCode extends Enumeration {
+import java.util
 
-  val OP_0 = Value(0)
+import scala.collection.mutable.ListBuffer
+
+object OpCode extends Enumeration {
+  val NEGATIVE_ZERO = 0x80.asInstanceOf[Byte]
+  val MAX_BYTE_AS_INT = 0x00ff
+  val MAX_BOSITIV_BYTE = 0x7f
+
+  abstract class OpValue(code: Int) {
+    def execute(stack: util.LinkedList[Array[Byte]]): Boolean
+  }
+
+  def encode(num: Long): Array[Byte] = {
+    require(num != null, "try to encode null")
+    if (num == 0)
+      return Array()
+
+    var abs_num = num.abs
+    val negative = num.signum < 0
+
+    var buffer = ListBuffer[Byte]()
+    while (abs_num != 0) {
+      buffer.+=((abs_num & MAX_BYTE_AS_INT).asInstanceOf[Byte])
+      abs_num >>= 8
+    }
+
+    if ((buffer.last & NEGATIVE_ZERO) != 0)
+      if (negative)
+        buffer.+=(NEGATIVE_ZERO)
+      else
+        buffer.+=(0)
+    else if (negative)
+      buffer(buffer.size - 1) = (buffer.last | NEGATIVE_ZERO).asInstanceOf[Byte]
+
+
+    buffer.toArray
+  }
+
+  def decode(bytes: Array[Byte]): Long = {
+    require(bytes != null, "try decode null")
+    if (bytes.isEmpty)
+      return 0
+
+    val big_endian = bytes.reverse
+    var negative: Boolean = true
+
+    var result = 0L
+    if ((big_endian(0) & NEGATIVE_ZERO) != 0) {
+      negative = true
+      result = big_endian(0) & MAX_BOSITIV_BYTE
+    } else {
+      negative = false
+      result = big_endian(0)
+    }
+
+    for (i <- 1 until big_endian.size) {
+      result <<= 8
+      result += big_endian(i) & MAX_BYTE_AS_INT
+    }
+
+    if (negative) -result else result
+  }
+
+  def OP_0 = new OpValue(0) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      stack.push(encode(0))
+      true
+    }
+  }
+
   val OP_FALSE = OP_0
 
   val OP_DATA_1 = Value(1)
@@ -95,54 +163,334 @@ object OpCode extends Enumeration {
   val OP_PUSHDATA2 = Value(77)
   val OP_PUSHDATA4 = Value(78)
 
-  val OP_1NEGATE = Value(79)
-  val OP_1 = Value(81)
-  val OP_TRUE = OP_1
-  val OP_2 = Value(82)
-  val OP_3 = Value(83)
-  val OP_4 = Value(84)
-  val OP_5 = Value(85)
-  val OP_6 = Value(86)
-  val OP_7 = Value(87)
-  val OP_8 = Value(88)
-  val OP_9 = Value(89)
-  val OP_10 = Value(90)
-  val OP_11 = Value(91)
-  val OP_12 = Value(92)
-  val OP_13 = Value(93)
-  val OP_14 = Value(94)
-  val OP_15 = Value(95)
-  val OP_16 = Value(96)
+  val OP_1NEGATE = new OpValue(79) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      stack.push(encode(-1))
+      true
+    }
+  }
+  val OP_1 = new OpValue(81) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      stack.push(encode(1))
+      true
+    }
+  }
 
+  val OP_TRUE = OP_1
+
+  val OP_2 = new OpValue(82) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      stack.push(encode(2))
+      true
+    }
+  }
+  val OP_3 = new OpValue(83) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      stack.push(encode(3))
+      true
+    }
+  }
+  val OP_4 = new OpValue(84) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      stack.push(encode(4))
+      true
+    }
+  }
+  val OP_5 = new OpValue(85) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      stack.push(encode(5))
+      true
+    }
+  }
+  val OP_6 = new OpValue(86) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      stack.push(encode(6))
+      true
+    }
+  }
+  val OP_7 = new OpValue(87) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      stack.push(encode(7))
+      true
+    }
+  }
+  val OP_8 = new OpValue(88) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      stack.push(encode(8))
+      true
+    }
+  }
+  val OP_9 = new OpValue(89) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      stack.push(encode(9))
+      true
+    }
+  }
+  val OP_10 = new OpValue(90) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      stack.push(encode(10))
+      true
+    }
+  }
+  val OP_11 = new OpValue(91) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      stack.push(encode(11))
+      true
+    }
+  }
+  val OP_12 = new OpValue(92) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      stack.push(encode(12))
+      true
+    }
+  }
+  val OP_13 = new OpValue(93) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      stack.push(encode(13))
+      true
+    }
+  }
+  val OP_14 = new OpValue(94) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      stack.push(encode(14))
+      true
+    }
+  }
+  val OP_15 = new OpValue(95) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      stack.push(encode(15))
+      true
+    }
+  }
+  val OP_16 = new OpValue(96) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      stack.push(encode(16))
+      true
+    }
+  }
   // control
-  val OP_NOP = Value(97)
+  val OP_NOP = new OpValue(97) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = true
+  }
+  val OP_VERIFY = new OpValue(105) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      if (stack.size() < 1)
+        false
+      val elem = stack.pop()
+      if (elem == 0)
+        false
+      true
+    }
+  }
+  val OP_RETURN = new OpValue(106) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = false
+  }
+
   val OP_IF = Value(99)
   val OP_NOTIF = Value(100)
   val OP_ELSE = Value(103)
   val OP_ENDIF = Value(104)
-  val OP_VERIFY = Value(105)
-  val OP_RETURN = Value(106)
-
   // stack ops
-  val OP_TOALTSTACK = Value(107)
-  val OP_FROMALTSTACK = Value(108)
-  val OP_2DROP = Value(109)
-  val OP_2DUP = Value(110)
-  val OP_3DUP = Value(111)
-  val OP_2OVER = Value(112)
-  val OP_2ROT = Value(113)
-  val OP_2SWAP = Value(114)
-  val OP_IFDUP = Value(115)
-  val OP_DEPTH = Value(116)
-  val OP_DROP = Value(117)
-  val OP_DUP = Value(118)
-  val OP_NIP = Value(119)
-  val OP_OVER = Value(120)
-  val OP_PICK = Value(121)
-  val OP_ROLL = Value(122)
-  val OP_ROT = Value(123)
-  val OP_SWAP = Value(124)
-  val OP_TUCK = Value(125)
+  val OP_TOALTSTACK = new OpValue(107) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = throw new NotImplementedError()
+
+    def execute(stack: util.Stack[Any], altstack: util.Deque[Any]): Boolean = {
+      if (stack.size() < 1)
+        return false
+      altstack.push(stack.pop())
+      true
+    }
+  }
+  val OP_FROMALTSTACK = new OpValue(108) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = throw new NotImplementedError()
+
+    def execute(stack: util.Stack[Any], altstack: util.Deque[Any]): Boolean = {
+      if (altstack.size() < 1)
+        return false
+      stack.push(altstack.pop())
+      true
+    }
+
+  }
+  val OP_2DROP = new OpValue(109) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      if (stack.size() < 2)
+        return false
+      stack.pop()
+      stack.pop()
+      true
+    }
+  }
+  val OP_2DUP = new OpValue(110) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      if (stack.size() < 2)
+        return false
+      val e0 = stack.get(0)
+      val e1 = stack.get(1)
+      stack.push(e1)
+      stack.push(e0)
+      true
+    }
+  }
+  val OP_3DUP = new OpValue(111) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      if (stack.size() < 3)
+        return false
+      val e0 = stack.get(0)
+      val e1 = stack.get(1)
+      val e2 = stack.get(2)
+      stack.push(e2)
+      stack.push(e1)
+      stack.push(e0)
+      true
+    }
+  }
+  val OP_2OVER = new OpValue(112) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      if (stack.size() < 4)
+        return false
+      val e2 = stack.get(2)
+      val e3 = stack.get(3)
+      stack.push(e3)
+      stack.push(e2)
+      true
+    }
+  }
+
+  val OP_2ROT = new OpValue(113) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      if (stack.size() < 6)
+        return false
+      val e4 = stack.remove(4)
+      val e5 = stack.remove(4)
+      stack.push(e5)
+      stack.push(e4)
+      true
+    }
+  }
+
+  val OP_2SWAP = new OpValue(114) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      if (stack.size() < 4)
+        return false
+      val e2 = stack.remove(2)
+      val e3 = stack.remove(2)
+      stack.push(e3)
+      stack.push(e2)
+      true
+    }
+  }
+  val OP_IFDUP = new OpValue(115) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      if (stack.size() < 1)
+        return false
+      if (decode(stack.peek()) != 0)
+        stack.push(stack.peek())
+      true
+    }
+  }
+  val OP_DEPTH = new OpValue(116) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      stack.push(encode(stack.size()))
+      true
+    }
+  }
+  val OP_DROP = new OpValue(117) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      if (stack.size() < 1)
+        return false
+      stack.pop()
+      true
+    }
+  }
+  val OP_DUP = new OpValue(118) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      if (stack.size() < 1)
+        return false
+      stack.push(stack.peek())
+      true
+    }
+  }
+  val OP_NIP = new OpValue(119) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      if (stack.size() < 2)
+        return false
+      stack.remove(1)
+      true
+    }
+  }
+  val OP_OVER = new OpValue(120) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      if (stack.size() < 2)
+        return false
+      stack.push(stack.get(1))
+      true
+    }
+  }
+  val OP_PICK = new OpValue(121) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      if (stack.size() < 1)
+        return false
+
+      val n = decode(stack.pop()).asInstanceOf[Int]
+      if (stack.size() < n + 1)
+        return false
+
+      stack.push(stack.get(n))
+      true
+    }
+  }
+  val OP_ROLL = new OpValue(122) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      if (stack.size() < 1)
+        return false
+
+      val n = decode(stack.pop()).asInstanceOf[Int]
+      if (stack.size() < n + 1)
+        return false
+
+      stack.push(stack.remove(n))
+      true
+    }
+  }
+  val OP_ROT = new OpValue(123) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      if (stack.size() < 3)
+        return false
+
+      val e0 = stack.pop()
+      val e1 = stack.pop()
+      val e2 = stack.pop()
+
+      stack.push(e1)
+      stack.push(e0)
+      stack.push(e2)
+      true
+    }
+  }
+  val OP_SWAP = new OpValue(124) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      if (stack.size() < 2)
+        return false
+
+      var e0 = stack.pop()
+      var e1 = stack.pop()
+      stack.push(e0)
+      stack.push(e1)
+      true
+    }
+  }
+  val OP_TUCK = new OpValue(125) {
+    override def execute(stack: util.LinkedList[Array[Byte]]): Boolean = {
+      if (stack.size() < 2)
+        return false
+
+      var e0 = stack.peek()
+      stack.add(2, e0)
+      true
+    }
+
+  }
 
 
   // splice ops
